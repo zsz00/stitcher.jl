e9964feafa14b436ab8051804259c6f78fb1dfc0
zsz00 <zsz6174@gmial.com>
2018-11-18 16:00:14 +0800

    update

--- stitch_2.jl
+++ stitch_2.jl
@@ -1,6 +1,5 @@
-# stitcher  
 # stitcher: Features match get H, then warp and stitch.  没有外参， 求出的H，就是外参集合.
-#  http://learningjulia.com/2018/08/25/image-stitching-part-2.html
+# http://learningjulia.com/2018/08/25/image-stitching-part-2.html
 using Images, ImageFeatures, FileIO, ImageView, ImageDraw, Colors;
 
 img1 = load("test/imgs/1.jpg")
