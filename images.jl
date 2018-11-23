4c70acd56c5b108bdaafe7083fe0726a4fddcf23
zsz00 <6174@gmail.com>
2018-11-23 21:11:23 +0800

    “updat”update

---
+++ images.jl
@@ -0,0 +1,12 @@
+# test images
+import Images, ImageFeatures, FileIO, ImageView, Dates
+# using CoordinateTransformations, StaticArrays, ImageTransformations, LinearAlgebra;
+
+time1 = Dates.now()
+img1 = FileIO.load("C:/Users/m1/test/src/imgs/4.jpg")  
+# [win10,小米笔记本, img:1280*720] no ImageMagick used 7s, 2018.11.23
+# img1 = Images.ImageMeta(img1, time="aaa")
+# println(img1["time"])
+# print("used: ", convert(Float64, Dates.now()-time1))
+print("used: ", (Dates.now()-time1).value/1000, " s")
+
---
+++ test_1.jl
@@ -0,0 +1,5 @@
+import Dates
+
+println(Dates.now())
+println(Dates.format(Dates.now(), "yyyy-mm-dd HH:MM:SS.s"))
+
