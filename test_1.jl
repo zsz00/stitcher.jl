dace19dfd17be976f8fb8d08a7af663fe342ef73
zsz00 <6174@gmail.com>
2018-11-25 13:42:52 +0800

    updat3e

--- test_1.jl
+++ test_1.jl
@@ -1,5 +1,10 @@
 import Dates
 
-println(Dates.now())
-println(Dates.format(Dates.now(), "yyyy-mm-dd HH:MM:SS.s"))
+# println(Dates.now())
+# println(Dates.format(Dates.now(), "yyyy-mm-dd HH:MM:SS.s"))
 
+ss = 1
+for i in 1:10
+    global ss += 1
+end
+println(ss)
